490ad5f5acd0ac0cc6987f7f5511d56a87c60249
Alston Lin <alstonlin812@gmail.com>
2016-03-26 11:02:57 -0400

    Basic webscraping is now functional

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+var request = require('request')
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+app.use(bodyParser());
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post('/find', function(req, res) {
+  var url = req.body.url;
+  console.log("URL: " + url);
+  request(url, function(er, response, body) {
+    console.log("Response " + body);
+    res.end(body);
+  }).end();
 });
 
 app.listen(3000, function () {
